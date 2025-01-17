0ab78c978297214521a6ca24b5ff916980579372
laurisvinters <lauris.vinters@proton.me>
2025-01-17 14:09:19 +0200

    Update

--- swedbank_krājrīks.py
+++ swedbank_krājrīks.py
@@ -8,21 +8,37 @@ logs.geometry("1000x600")
 swedbank_interest = 2.5
 
 def calculate():
-    allocated = int(iemaksa_entry.get())
-    procenta_summa = allocated * (swedbank_interest / 100)
-    if procenta_summa % 1 >= 0.5:
-        procenta_summa = math.ceil(procenta_summa)
-    else:
-        procenta_summa = math.floor(procenta_summa)
+    vienreizējā_iemaksa = int(iemaksa_entry.get())
+    ikmenesa_iemaksa = int(ikmenesa_iemaksa_entry.get())
+    
+    ikmenesa_iemaksa_value.config(text=str(ikmenesa_iemaksa * 12 + vienreizējā_iemaksa))
+    
+
+    procenta_summa = vienreizējā_iemaksa * (swedbank_interest / 100)
+
+    monthly_sum = 0
+    for month in range(11, -1, -1):
+        monthly_interest = ikmenesa_iemaksa * (swedbank_interest / 100) * ((month + 1) / 12)
+        monthly_sum += monthly_interest
+    
+    procenta_summa += monthly_sum
+    
+    procenta_summa = round(procenta_summa, 2)
+    procenta_summa = math.ceil(procenta_summa) if procenta_summa % 1 >= 0.5 else math.floor(procenta_summa)
     
     procenta_summa_value.config(text=str(procenta_summa))
-
 
 iemaksa_entry = Entry(logs, font=("Arial", 16))
 iemaksa_entry.pack(pady=20)
 
 iemaksas_label = Label(logs, text="Iemaksa", font=("Arial", 16))
 iemaksas_label.pack(pady=20)
+
+ikmenesa_iemaksa_entry = Entry(logs, font=("Arial", 16))
+ikmenesa_iemaksa_entry.pack(pady=20)
+
+ikmenesas_label = Label(logs, text="Ikmenesā iemaksa", font=("Arial", 16))
+ikmenesas_label.pack(pady=20)
 
 calculate_button = Button(logs, text="Aprēķināt", font=("Arial", 16), command=calculate)
 calculate_button.pack(pady=20)
@@ -32,5 +48,11 @@ procenta_summa.pack(pady=20)
 
 procenta_summa_value = Label(logs, text="0", font=("Arial", 16))
 procenta_summa_value.pack(pady=20)
+
+ikmenesa_iemaksa = Label(logs, text="Manas Iemaksas", font=("Arial", 16))
+ikmenesa_iemaksa.pack(pady=20)
+
+ikmenesa_iemaksa_value = Label(logs, text="0", font=("Arial", 16))
+ikmenesa_iemaksa_value.pack(pady=20)
 
 logs.mainloop()
