d825f4ae01b1bfcf9775877eaee1f7951e4c8b42
laurisvinters <lauris.vinters@proton.me>
2025-01-17 14:18:28 +0200

    Dizaina maiņas

--- swedbank_krājrīks.py
+++ swedbank_krājrīks.py
@@ -13,7 +13,6 @@ def calculate():
     
     ikmenesa_iemaksa_value.config(text=str(ikmenesa_iemaksa * 12 + vienreizējā_iemaksa))
     
-
     procenta_summa = vienreizējā_iemaksa * (swedbank_interest / 100)
 
     monthly_sum = 0
@@ -28,28 +27,36 @@ def calculate():
     
     procenta_summa_value.config(text=str(procenta_summa))
 
-iemaksa_entry = Entry(logs, font=("Arial", 16))
+main_frame = Frame(logs)
+main_frame.pack(fill="both", expand=True)
+
+left_frame = Frame(main_frame)
+left_frame.pack(side="left", fill="both", expand=True)
+right_frame = Frame(main_frame)
+right_frame.pack(side="right", fill="both", expand=True)
+
+iemaksa_entry = Entry(left_frame, font=("Arial", 16))
 iemaksa_entry.pack(pady=20)
 
-iemaksas_label = Label(logs, text="Iemaksa", font=("Arial", 16))
+iemaksas_label = Label(left_frame, text="Iemaksa", font=("Arial", 16))
 iemaksas_label.pack(pady=20)
 
-ikmenesa_iemaksa_entry = Entry(logs, font=("Arial", 16))
+ikmenesa_iemaksa_entry = Entry(left_frame, font=("Arial", 16))
 ikmenesa_iemaksa_entry.pack(pady=20)
 
-ikmenesas_label = Label(logs, text="Ikmenesā iemaksa", font=("Arial", 16))
+ikmenesas_label = Label(left_frame, text="Ikmenesā iemaksa", font=("Arial", 16))
 ikmenesas_label.pack(pady=20)
 
-calculate_button = Button(logs, text="Aprēķināt", font=("Arial", 16), command=calculate)
+calculate_button = Button(right_frame, text="Aprēķināt", font=("Arial", 16), command=calculate)
 calculate_button.pack(pady=20)
 
-procenta_summa = Label(logs, text="Procenta summa", font=("Arial", 16))
+procenta_summa = Label(right_frame, text="Procenta summa", font=("Arial", 16))
 procenta_summa.pack(pady=20)
 
-procenta_summa_value = Label(logs, text="0", font=("Arial", 16))
+procenta_summa_value = Label(right_frame, text="0", font=("Arial", 16))
 procenta_summa_value.pack(pady=20)
 
-ikmenesa_iemaksa = Label(logs, text="Manas Iemaksas", font=("Arial", 16))
+ikmenesa_iemaksa = Label(right_frame, text="Manas Iemaksas", font=("Arial", 16))
 ikmenesa_iemaksa.pack(pady=20)
 
 ikmenesa_iemaksa_value = Label(logs, text="0", font=("Arial", 16))
