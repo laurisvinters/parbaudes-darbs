aa8db31adb257360d3e9658b2019ad335c68c55c
laurisvinters <lauris.vinters@proton.me>
2025-01-17 10:54:29 +0200

    Pāreja uz TKinter, izprintē konsolē

--- swedbank_krājrīks.py
+++ swedbank_krājrīks.py
@@ -1,15 +1,26 @@
 from datetime import date
 from tkinter import *
 
-allocated = 0
-today = date.today()
+logs = Tk()
+logs.title("Swedbank krājrīks")
+logs.geometry("1000x600")
+
 swedbank_interest = 2.5
+today = date.today()
 
-def accured_interest():
-    return allocated * (swedbank_interest / 100)
-
-allocated = int(input("Ievadi cik krājkontā jums ir naudas: "))
-
-print("Jums ir {} krājkontā, un ar swedbank interesi jūs pelnīsiet {} gadā.".format(allocated, accured_interest()))
+def calculate():
+    allocated = int(iemaksa_entry.get())
+    print("Jums ir {} krājkontā, un ar swedbank interesi jūs pelnīsiet {} gadā.".format(allocated, allocated * (swedbank_interest / 100)))
 
 
+
+iemaksa_entry = Entry(logs, font=("Arial", 16))
+iemaksa_entry.pack(pady=20)
+
+iemaksas_label = Label(logs, text="Iemaksa", font=("Arial", 16))
+iemaksas_label.pack(pady=20)
+
+calculate_button = Button(logs, text="Aprēķināt", font=("Arial", 16), command=calculate)
+calculate_button.pack(pady=20)
+
+logs.mainloop()
